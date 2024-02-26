d36d0348209262a0d9b77ef9225d67ad8f53c163
AlexeyDevTech <108923810+AlexeyDevTech@users.noreply.github.com>
2024-02-26 19:03:09 +0300

    initial

---
+++ Chains.Core/ChainBuilder.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chains.Core
+{
+    
+    public class StateManager
+    {
+        public State Root { get; set; }
+
+        public State CreateInitialState(string name)
+        {
+            var r = new State(name);
+            Root = r;
+            return r;
+        }
+        public void Active(State state) => state.Active = true;
+    }
+    public class State
+    {
+        private bool _active;
+
+        internal State Parent { get; set; }
+        internal Action OnEnter { get; set; }
+        internal Action OnLeave { get; set; }
+        internal Action OnUpdate { get; set; }
+        internal bool Active 
+        { 
+            get
+            {
+                return _active;
+
+            }
+            set
+            {
+                _active = value;
+                if (value)
+                {
+                    if(Parent != null)
+                     Parent.Active = false;
+                    Enter();
+                }
+                else
+                    Leave();
+            }
+        }
+
+        public string Name { get; set; } = "New State";
+        internal List<Condition> Conditions { get; set; }
+
+        public State(string Name)
+        {
+            this.Name = Name;
+            Conditions = new List<Condition>();
+        }
+      
+        internal void AddCondition(Condition condition) => Conditions.Add(condition);
+        public void UpdateState()
+        {
+            for(int i = 0; i < Conditions.Count; i++)
+            {
+                Conditions[i].Update();       
+            }
+            OnUpdate?.Invoke();
+        }
+        internal void SetUpdateAction(Action action) => OnUpdate = action;
+        internal void SetEnterAction(Action action) => OnEnter = action;
+        internal void SetLeaveAction(Action action) => OnLeave = action;
+        internal void Enter() => OnEnter?.Invoke();
+        internal void Leave() => OnLeave?.Invoke();
+
+        public override string ToString()
+        {
+            return $"State {Name}";
+        }
+
+    }
+    public class Condition
+    {
+        public Func<bool> Predicate { get; set; }
+        public Action Action { get; set; }
+        public State Next { get; set; }
+        public void Update()
+        {
+            
+            if (Predicate?.Invoke() ?? true)
+            {
+                Action?.Invoke();
+                Next.Active = true;
+            }
+
+        }
+
+    }
+
+    public static class StateExtensions
+    {
+        public static State OnState(this State parent, string Name)
+        {
+            var s = new State(Name);
+            s.Parent = parent;
+            return s;
+        }
+        public static State Enter(this State state, Action action)
+        {
+            state.SetEnterAction(action);
+            return state;
+        }
+        public static State Leave(this State state, Action action)
+        {
+            state.SetLeaveAction(action);
+            return state;
+        }
+        public static State Update(this State state, Action action)
+        {
+            state.SetUpdateAction(action);
+            return state;
+        }
+        public static State If(this State state, Func<bool> predict, Action action)
+        {
+            state.AddCondition(new Condition { Predicate = predict, Action = action });
+            return state;
+        }
+        public static State Active(this State state)
+        {
+            state.Active = true;
+            return state;
+        }
+        
+
+    }
+}
---
+++ Chains/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Chains.Core;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chains.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private double _trValue;
+        State s;
+        public MainViewModel()
+        {
+            s = new StateManager().CreateInitialState("Root");
+            s.Enter(() => Debug.WriteLine(">>Root enter action"))
+             .Update(() => Debug.WriteLine(">>Root update action"))
+             .Leave(() => Debug.WriteLine(">>Root leave action"));
+            s.Active();
+            
+        }
+
+        public double TRValue 
+        { 
+            get => _trValue;
+            set
+            {
+                SetProperty(ref _trValue, Math.Round(value, 2));
+                s?.UpdateState();
+                Debug.WriteLine($"{_trValue}");
+            }
+        }
+    }
+}
